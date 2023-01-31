9b2c3365fc0d98cbee37bd9b642fe2123df0ec95
Moritz Hesche <mo.hesche@gmail.com>
2023-01-31 08:40:38 +0100

    basic processing pipeline

--- contact_graspnet_benchmark/dataloading/datasets.py
+++ contact_graspnet_benchmark/dataloading/datasets.py
@@ -1,7 +1,34 @@
 from pathlib import Path
 
+import numpy as np
+
+from contact_graspnet_benchmark.datatypes import CameraData
+
+
 class OrigExampleDataset:
     def __init__(self, dataset_dir: Path):
         self.dataset_dir = dataset_dir
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
+        segmap, rgb, depth, pc_full, pc_colors = None, None, None, None, None
+
+        # if K is not None:
+        #     if isinstance(K,str):
+        #         cam_K = eval(K)
+        #     cam_K = np.array(K).reshape(3,3)
+
+        # if '.np' in p:
+        sample_path = self.dataset_dir / f"{idx}.npy"
+        assert sample_path.exists()
+
+        data = np.load(sample_path, allow_pickle=True)
+        data = data.item()
+
+        return CameraData(
+            rgb=data["rgb"],
+            depth=data["depth"],
+            points=None,  # data["xyz"],
+            points_colors=None,  # data["xyz_color"]
+            segmentation=data["seg"],
+            name=f"exaple_{idx}",
+        )
--- contact_graspnet_benchmark/datatypes/datatypes.py
+++ contact_graspnet_benchmark/datatypes/datatypes.py
@@ -13,10 +13,15 @@ class CameraData:
     rgb: NDArray[Shape["3,h,w"], Float]
     depth: NDArray[Shape["1,h,w"], Float]
     points: NDArray[Shape["n,3"], Float]
-    segmentation: NDArray[Shape["1,h,w"], Float]
+    points_colors: NDArray[Shape["n,3"], Float]
+    segmentation: NDArray[Shape["1,h,w"], Float]  # not binary necessarily
     name: str
-    pos_grasps: NDArray[Shape["n_pos_grasps,4,2"], Float] = None
-    neg_grasps: NDArray[Shape["n_pos_grasps,4,2"], Float] = None
     cam_intrinsics: NDArray[Shape["3, 3"], Float] = None
     cam_pos: NDArray[Shape["3"], Float] = None
     cam_rot: NDArray[Shape["4"], Float] = None
+
+
+@dataclass
+class Grasp:
+    grasp_cam: NDArray
+    # TODO
