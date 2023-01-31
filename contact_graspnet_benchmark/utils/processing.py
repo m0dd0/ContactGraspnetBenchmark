eb13910456fd7d658d15f6a1b81b39eaa00c0fb0
Moritz Hesche <mo.hesche@gmail.com>
2023-01-31 15:17:06 +0100

    more basic files

--- contact_graspnet_benchmark/utils/processing.py
+++ contact_graspnet_benchmark/utils/processing.py
@@ -13,216 +13,39 @@ from contact_graspnet_benchmark.datatypes import CameraData
 class End2EndProcessor:
     def __init__(
         self,
-        checkpoint_dir: Path,
+        checkpoint_dir: Path = None,
         forward_passes: int = 1,
     ):
-        self.global_config: Dict[str, Any] = config_utils.load_config(
-            checkpoint_dir, batch_size=forward_passes
-        )
+        self.forward_passes = forward_passes
 
-        self.grasp_estimator = GraspEstimator(self.global_config)
+        # setup grasp estimator
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        config.allow_soft_placement = True
+        sess = tf.Session(config=config)
+        self.grasp_estimator = GraspEstimator(
+            config_utils.load_config(checkpoint_dir, batch_size=self.forward_passes),
+            sess,
+        )
         self.grasp_estimator.build_network()
-
-        self.saver = tf.train.Saver(save_relative_paths=True)
-
-        self.config = tf.ConfigProto()
-        self.config.gpu_options.allow_growth = True
-        self.config.allow_soft_placement = True
-        self.sess = tf.Session(config=self.config)
-
         self.grasp_estimator.load_weights(
-            self.sess, self.saver, self.checkpoint_dir, mode="test"
+            tf.train.Saver(save_relative_paths=True),
+            checkpoint_dir,
+            mode="test",
         )
 
-    def __call__(self, sample: CameraData):
+    def __call__(self, points):
         (
             pred_grasps_cam,
             scores,
             contact_pts,
             _,
         ) = self.grasp_estimator.predict_scene_grasps(
-            self.sess,
-            sample.points,
+            points,
             # pc_segments=pc_segments,
             # local_regions=local_regions,
             # filter_grasps=filter_grasps,
             forward_passes=self.forward_passes,
         )
 
-
-# def inference(
-#     global_config,
-#     checkpoint_dir,
-#     input_paths,
-#     K=None,
-#     local_regions=True,
-#     skip_border_objects=False,
-#     filter_grasps=True,
-#     segmap_id=None,
-#     z_range=[0.2, 1.8],
-#     forward_passes=1,
-# ):
-#     """
-#     Predict 6-DoF grasp distribution for given model and input data
-
-#     :param global_config: config.yaml from checkpoint directory
-#     :param checkpoint_dir: checkpoint directory
-#     :param input_paths: .png/.npz/.npy file paths that contain depth/pointcloud and optionally intrinsics/segmentation/rgb
-#     :param K: Camera Matrix with intrinsics to convert depth to point cloud
-#     :param local_regions: Crop 3D local regions around given segments.
-#     :param skip_border_objects: When extracting local_regions, ignore segments at depth map boundary.
-#     :param filter_grasps: Filter and assign grasp contacts according to segmap.
-#     :param segmap_id: only return grasps from specified segmap_id.
-#     :param z_range: crop point cloud at a minimum/maximum z distance from camera to filter out outlier points. Default: [0.2, 1.8] m
-#     :param forward_passes: Number of forward passes to run on each point cloud. Default: 1
-#     """
-
-#     # Build the model
-#     grasp_estimator = GraspEstimator(global_config)
-#     grasp_estimator.build_network()
-
-#     # Add ops to save and restore all the variables.
-#     saver = tf.train.Saver(save_relative_paths=True)
-
-#     # Create a session
-#     config = tf.ConfigProto()
-#     config.gpu_options.allow_growth = True
-#     config.allow_soft_placement = True
-#     sess = tf.Session(config=config)
-
-#     # Load weights
-#     grasp_estimator.load_weights(sess, saver, checkpoint_dir, mode="test")
-
-#     os.makedirs("results", exist_ok=True)
-
-#     # Process example test scenes
-#     for p in glob.glob(input_paths):
-#         print("Loading ", p)
-
-#         pc_segments = {}
-#         segmap, rgb, depth, cam_K, pc_full, pc_colors = load_available_input_data(
-#             p, K=K
-#         )
-
-#         # if segmap is None and (local_regions or filter_grasps):
-#         #     raise ValueError(
-#         #         "Need segmentation map to extract local regions or filter grasps"
-#         #     )
-
-#         # if pc_full is None:
-#         #     print("Converting depth to point cloud(s)...")
-#         #     pc_full, pc_segments, pc_colors = grasp_estimator.extract_point_clouds(
-#         #         depth,
-#         #         cam_K,
-#         #         segmap=segmap,
-#         #         rgb=rgb,
-#         #         skip_border_objects=skip_border_objects,
-#         #         z_range=z_range,
-#         #     )
-
-#         print("Generating Grasps...")
-#         pred_grasps_cam, scores, contact_pts, _ = grasp_estimator.predict_scene_grasps(
-#             sess,
-#             pc_full,
-#             pc_segments=pc_segments,
-#             local_regions=local_regions,
-#             filter_grasps=filter_grasps,
-#             forward_passes=forward_passes,
-#         )
-
-#         # Save results
-#         np.savez(
-#             "results/predictions_{}".format(
-#                 os.path.basename(p.replace("png", "npz").replace("npy", "npz"))
-#             ),
-#             pred_grasps_cam=pred_grasps_cam,
-#             scores=scores,
-#             contact_pts=contact_pts,
-#         )
-
-#         # Visualize results
-#         # show_image(rgb, segmap)
-#         # visualize_grasps(
-#         #     pc_full, pred_grasps_cam, scores, plot_opencv_cam=True, pc_colors=pc_colors
-#         # )
-
-#     if not glob.glob(input_paths):
-#         print("No files found: ", input_paths)
-
-
-# if __name__ == "__main__":
-
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument(
-#         "--np_path",
-#         default="test_data/7.npy",
-#         help='Input data: npz/npy file with keys either "depth" & camera matrix "K" or just point cloud "pc" in meters. Optionally, a 2D "segmap"',
-#     )
-#     parser.add_argument(
-#         "--K",
-#         default=None,
-#         help='Flat Camera Matrix, pass as "[fx, 0, cx, 0, fy, cy, 0, 0 ,1]"',
-#     )
-#     parser.add_argument(
-#         "--z_range",
-#         default=[0.2, 1.8],
-#         help="Z value threshold to crop the input point cloud",
-#     )
-#     parser.add_argument(
-#         "--local_regions",
-#         action="store_true",
-#         default=False,
-#         help="Crop 3D local regions around given segments.",
-#     )
-#     parser.add_argument(
-#         "--filter_grasps",
-#         action="store_true",
-#         default=False,
-#         help="Filter grasp contacts according to segmap.",
-#     )
-#     parser.add_argument(
-#         "--skip_border_objects",
-#         action="store_true",
-#         default=False,
-#         help="When extracting local_regions, ignore segments at depth map boundary.",
-#     )
-#     parser.add_argument(
-#         "--forward_passes",
-#         type=int,
-#         default=1,
-#         help="Run multiple parallel forward passes to mesh_utils more potential contact points.",
-#     )
-#     parser.add_argument(
-#         "--segmap_id",
-#         type=int,
-#         default=0,
-#         help="Only return grasps of the given object id",
-#     )
-#     parser.add_argument(
-#         "--arg_configs",
-#         nargs="*",
-#         type=str,
-#         default=[],
-#         help="overwrite config parameters",
-#     )
-#     FLAGS = parser.parse_args()
-
-#     global_config = config_utils.load_config(
-#         FLAGS.ckpt_dir, batch_size=FLAGS.forward_passes, arg_configs=FLAGS.arg_configs
-#     )
-
-#     print(str(global_config))
-#     print("pid: %s" % (str(os.getpid())))
-
-#     inference(
-#         global_config,
-#         FLAGS.ckpt_dir,
-#         FLAGS.np_path if not FLAGS.png_path else FLAGS.png_path,
-#         z_range=eval(str(FLAGS.z_range)),
-#         K=FLAGS.K,
-#         local_regions=FLAGS.local_regions,
-#         filter_grasps=FLAGS.filter_grasps,
-#         segmap_id=FLAGS.segmap_id,
-#         forward_passes=FLAGS.forward_passes,
-#         skip_border_objects=FLAGS.skip_border_objects,
-#     )
+        return pred_grasps_cam, scores, contact_pts
