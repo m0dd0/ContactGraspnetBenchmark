71b55bea80d3c5435ecf231a710e1261daf7b7c8
Moritz Hesche <mo.hesche@gmail.com>
2023-02-18 15:50:02 +0100

    adapted module template code for tf

--- contact_graspnet/models/custom_modules.py
+++ contact_graspnet/models/custom_modules.py
@@ -1,13 +1,11 @@
 """This module contains submodules of the network architecture(s) like special layers etc."""
 
-import torch.nn as nn
-
 from torchtyping import TensorType
 
 
-class CustomModuleExample(nn.Module):
-    def __init__(self):
-        super().__init__()
+# class CustomModuleExample:
+#     def __init__(self):
+#         super().__init__()
 
-    def forward(self, x: TensorType) -> TensorType:
-        raise NotImplementedError()
+#     def forward(self, x: TensorType) -> TensorType:
+#         raise NotImplementedError()
