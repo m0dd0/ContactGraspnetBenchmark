469753dfccce49af2e08a3dc06fa528c55d40f7e
Moritz Hesche <mo.hesche@gmail.com>
2023-12-05 00:12:55 +0100

    remvoing dependencies

--- setup.py
+++ setup.py
@@ -13,23 +13,23 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     # python version == 3.7.9
     install_requires=[
-        "numpy",
-        # "opencv-python",
-        "opencv-python-headless",
-        "tensorflow",
-        "pyyaml==5.3.1",
-        "Pillow",
-        "trimesh",
-        "scipy",
-        "pyrender==0.1.43",
-        "pyglet==1.5.9",
-        "tqdm",
-        "matplotlib",
-        # the dependencies below have been added during refactoring
-        "nptyping",  # for tf tensors we use string annotations for now
-        "appdirs",
-        "tabulate",
-        "scikit-image",
+        # "numpy",
+        # # "opencv-python",
+        # "opencv-python-headless",
+        # "tensorflow",
+        # "pyyaml==5.3.1",
+        # "Pillow",
+        # "trimesh",
+        # "scipy",
+        # "pyrender==0.1.43",
+        # "pyglet==1.5.9",
+        # "tqdm",
+        # "matplotlib",
+        # # the dependencies below have been added during refactoring
+        # "nptyping",  # for tf tensors we use string annotations for now
+        # "appdirs",
+        # "tabulate",
+        # "scikit-image",
     ],
     extras_require={
         "dev": ["black", "pylint", "jupyter", "ipykernel", "ipympl"],
